76eb0cae51b73b55f6508e808d64ed65b010573e
Edwino Stein <edwino.stein@gmail.com>
2016-08-21 11:32:42 -0400

    Adicionando classes para abstração do cliente e do servidor

---
+++ core/Client.java
@@ -0,0 +1,110 @@
+package core;
+
+import rmi.interfaces.PeerInterface;
+import rmi.interfaces.PeerInfoInterface;
+import rmi.interfaces.MessageInterface;
+import rmi.PeerInfo;
+import rmi.Message;
+
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.Remote;
+
+public class Client {
+
+    protected PeerInterface stub;
+    protected Exception lastException;
+
+    public Client(PeerInterface stub){
+        this.stub = stub;
+        this.lastException = null;
+    }
+
+    public String getUUID(){
+
+        try {
+
+            if(this.stub.isAlive()){
+                String uuid = this.stub.getUUID();
+                return uuid;
+            }
+
+        } catch (Exception e) {
+            this.lastException = e;
+            return null;
+        }
+
+        return null;
+    }
+
+    public PeerInfo getInfo(){
+
+        try {
+
+            if(this.stub.isAlive()){
+                PeerInfoInterface info = this.stub.getInfo();
+                return (PeerInfo) info;
+            }
+
+        } catch (Exception e) {
+            this.lastException = e;
+            return null;
+        }
+
+        return null;
+    }
+
+    public boolean send(PeerInfoInterface from, MessageInterface msg){
+
+        try {
+
+            if(this.stub.isAlive()){
+                return this.stub.send(from, msg);
+            }
+
+        } catch (Exception e) {
+            this.lastException = e;
+            return false;
+        }
+
+        return false;
+    }
+
+    public boolean send(PeerInfoInterface from, String msg){
+        return this.send(from, new Message(msg));
+    }
+
+
+    public boolean isAlive(){
+
+        try {
+            return this.stub.isAlive();
+        } catch (Exception e) {
+            this.lastException = e;
+            return false;
+        }
+    }
+
+    public Exception getLastException(){
+        return this.lastException;
+    }
+
+    public static Client fetchPeer(String server, int port){
+
+        PeerInterface stub = null;
+
+        try {
+            Registry registry = LocateRegistry.getRegistry(server, port);
+            stub = (PeerInterface) registry.lookup("Peer");
+            if(stub.isAlive()) return new Client(stub);
+
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+            return null;
+        }
+
+        return null;
+    }
+}
---
+++ core/Server.java
@@ -0,0 +1,48 @@
+package core;
+
+import rmi.interfaces.PeerInterface;
+import rmi.Peer;
+import rmi.PeerInfo;
+import rmi.Message;
+
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.Remote;
+
+public class Server {
+
+    protected int port;
+    protected Peer peer;
+    protected PeerInfo info;
+    protected Registry registry;
+
+    public Server(PeerInfo info, int port){
+
+        this.info = info;
+        this.port = port;
+        this.registry = null;
+
+        try {
+            this.peer = new Peer(info);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run(){
+
+        try {
+
+            UnicastRemoteObject.unexportObject(this.peer, true);
+            PeerInterface stub = (PeerInterface) UnicastRemoteObject.exportObject(this.peer, this.port);
+            this.registry = LocateRegistry.createRegistry(this.port);
+            this.registry.bind("Peer", stub);
+
+            System.out.println("O servidor está em execução");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
